255bbebc62871ab6eb13538402210cb0783cf373
CCherry07 <2405693142@qq.com>
2022-07-20 22:15:23 +0800

    feat:  015. 三数之和

--- 015. 三数之和.ts
+++ 015. 三数之和.ts
@@ -3,12 +3,12 @@ function threeSum(nums: number[]): number[][] {
   let resArr:number[][]=[]
   nums.sort((a,b)=>a-b)
   for(let i=0;i<nums.length-2;i++){
-    if(i>0 && nums[i]==nums[i-1]) continue;
+    if(i>0 && nums[i]===nums[i-1]) continue;
     let left = i+1;
     let right = nums.length-1;
     while(left<right){
       let sum = nums[i]+nums[left]+nums[right];
-      if(sum==0){
+      if(sum===0){
         resArr.push([nums[i],nums[left],nums[right]])
         left++;
         right--;
