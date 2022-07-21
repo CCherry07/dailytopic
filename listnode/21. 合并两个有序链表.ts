1e7f7c3a97f754df041e6c4d0ee3ea64f7a26439
CCherry07 <2405693142@qq.com>
2022-07-21 14:49:29 +0800

    feat:  solution 2

--- listnode/21. 合并两个有序链表.ts
+++ listnode/21. 合并两个有序链表.ts
@@ -27,3 +27,16 @@ import { ListNode } from './type'
   cur.next = list1 ?? list2;
   return res.next
 };
+
+// two
+const mergeTwoLists2 = function (list1: ListNode | null, list2: ListNode | null): ListNode | null {
+  if (!list1) return list2
+  if (!list2) return list1
+  if (list1.val < list2.val) {
+      list1.next = mergeTwoLists(list1.next, list2)
+      return list1
+  } else {
+      list2.next = mergeTwoLists(list1, list2.next)
+      return list2
+  }
+}
