60c8b3ef2f002104e9fb28681e3942131edde1cd
CCherry07 <2405693142@qq.com>
2022-10-15 23:59:38 +0800

    feat:  1441. 用栈操作构建数组

---
+++ 1441. 用栈操作构建数组.ts
@@ -0,0 +1,22 @@
+function buildArray(target: number[], n: number): string[] {
+  const result: string[] = []
+  let targetIdx = 0
+  for (let i = 1; i <= n; i++) {
+    if (targetIdx >= target.length) break
+    if (i !== target[targetIdx]) {
+      result.push("Push")
+      result.push("Pop")
+      continue
+    } else {
+      result.push("Push")
+      targetIdx += 1
+      continue
+    }
+  }
+  return result
+};
+
+let target = [1, 3], n = 3
+
+console.log(buildArray(target, n));
+
