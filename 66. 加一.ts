64d8950ee53a6e4cca739832c75ea0e85ea0ba24
CCherry07 <2405693142@qq.com>
2022-10-18 08:54:30 +0800

    feat:  case 2

--- 66. 加一.ts
+++ 66. 加一.ts
@@ -10,8 +10,24 @@ function plusOne(digits: number[]): number[] {
       } else {
         isAdd = false
       }
+    } else {
+      break
     }
   }
   if (isAdd) digits.unshift(1)
+  return digits
+};
+
+
+// 2
+
+function plusOne2(digits: number[]): number[] {
+  const len = digits.length - 1
+  for (let i = len; i >= 0; i--) {
+    digits[i]++;
+    digits[i] %= 10;
+    if (digits[i] != 0) return digits
+  }
+  digits.unshift(1)
   return digits
 };
