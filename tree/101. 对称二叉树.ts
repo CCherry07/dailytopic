f8028d95645e0ba48df2f49277c200b3e8103859
CCherry07 <2405693142@qq.com>
2022-10-23 22:52:21 +0800

    feat:  101. 对称二叉树

---
+++ tree/101. 对称二叉树.ts
@@ -0,0 +1,11 @@
+import { TreeNode } from './type';
+function isSymmetric(root: TreeNode | null): boolean {
+  if (root === null) return true
+  function def(left: TreeNode | null, right: TreeNode | null) {
+    if (left === null && right === null) return true
+    if (left === null || right === null) return false
+    if (left.val !== right.val) return false
+    return def(left.left, right.right) && def(left.right, right.left)
+  }
+  return def(root.left, root.right)
+};
