0f8ba9906c1302c4d2f2330e4203559e6f0c9512
CCherry07 <2405693142@qq.com>
2022-10-24 22:40:27 +0800

    feat:  112. 路径总和

---
+++ tree/112. 路径总和.ts
@@ -0,0 +1,11 @@
+// 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+// 如果存在，返回 true ；否则，返回 false 。
+// 叶子节点 是指没有子节点的节点。
+import { TreeNode } from "./type"
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  if (!root) return false
+  if (!root.left && !root.right) {
+    return targetSum === root.val
+  }
+  return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+};
