d6c1adc9474341a5124a59b2f05bea3f9d873e67
CCherry7 <2405693142@qq.com>
2022-11-14 17:12:52 +0800

    feat:  343 整数拆分

---
+++ 343. 整数拆分.ts
@@ -0,0 +1,12 @@
+function integerBreak(n: number): number {
+  const dp: number[] = new Array(n + 1).fill(0);
+  dp[2] = 1;
+  for (let i = 3; i <= n; i++) {
+    for (let j = 0; j <= i / 2; j++) {
+      dp[i] = Math.max(dp[i], j * dp[i - j], j * (i - j));
+    }
+  }
+  return dp[n]
+};
+
+integerBreak(10)
