bfabd6cab8f1a04467184d614ad01b87491d94a1
CCherry7 <2405693142@qq.com>
2022-12-12 22:06:51 +0800

    git commit -m '977 有序数组的平方2'

--- 977. 有序数组的平方.ts
+++ 977. 有序数组的平方.ts
@@ -1,3 +1,21 @@
 function sortedSquares(nums: number[]): number[] {
   return nums.map(num => num * num).sort((a, b) => a - b)
 };
+function sortedSquares2(nums: number[]): number[] {
+  const result: number[] = []
+  let left = 0, right = nums.length - 1
+  let current = right
+  while (left <= right) {
+    const r1 = nums[left] * nums[left]
+    const r2 = nums[right] * nums[right]
+    if (r1 > r2) {
+      result[current] = r1
+      left++
+    } else {
+      result[current] = r2
+      right--
+    }
+    current--
+  }
+  return result
+};
