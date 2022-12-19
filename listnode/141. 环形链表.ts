f5490339ac8b0f7bc6f6d7a13f4621674f9fe2c0
CCherry07 <2405693142@qq.com>
2022-12-19 22:28:53 +0800

    feat: hasCycle3

--- listnode/141. 环形链表.ts
+++ listnode/141. 环形链表.ts
@@ -2,28 +2,28 @@ class ListNode {
     val: number
     next: ListNode | null
     constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
     }
 }
 
 // solution 1
 function hasCycle(head: ListNode | null): boolean {
-  if(!head?.next){
-      return false
-  }
-  const nodeSet = new Set<ListNode>()
-  while(head){
-      if(nodeSet.has(head))return true
-      nodeSet.add(head)
-      head = head.next
-  }
-  return false
+    if (!head?.next) {
+        return false
+    }
+    const nodeSet = new Set<ListNode>()
+    while (head) {
+        if (nodeSet.has(head)) return true
+        nodeSet.add(head)
+        head = head.next
+    }
+    return false
 };
 
 // solution 2
 function hasCycle2(head: ListNode | null): boolean {
-    if(!head?.next){
+    if (!head?.next) {
         return false
     }
     let slow = head as ListNode | null
@@ -34,6 +34,28 @@ function hasCycle2(head: ListNode | null): boolean {
         if (fast === slow) return true
     }
     return false
-  };
+};
 
-export {}
+// solution 3
+
+function hasCycle3(head: ListNode | null): ListNode | null {
+    if (!head?.next) {
+        return null
+    }
+    let slow = head as ListNode | null
+    let fast = head as ListNode | null
+    while (fast && fast.next) {
+        fast = fast.next.next
+        slow = slow!.next
+        if (fast === slow) {
+            slow = head
+            while (slow !== fast) {
+                slow = slow!.next
+                fast = fast!.next
+            }
+            return slow
+        }
+    }
+    return null
+}
+export { }
