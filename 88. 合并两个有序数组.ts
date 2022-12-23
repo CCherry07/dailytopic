27ffddd64e5e397830e460eded54b9f2bfd0a31c
CCherry07 <2405693142@qq.com>
2022-12-23 22:31:31 +0800

    22: o1

--- 88. 合并两个有序数组.ts
+++ 88. 合并两个有序数组.ts
@@ -14,3 +14,24 @@ let nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3
 merge(nums1, m, nums2, n)
 
 console.log(nums1);
+
+function merge2(nums1: number[], m: number, nums2: number[], n: number): void {
+  let i = m - 1
+  let j = n - 1
+  let k = m + n - 1
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i]
+      i--
+    } else {
+      nums1[k] = nums2[j]
+      j--
+    }
+    k--
+  }
+  while (j >= 0) {
+    nums1[k] = nums2[j]
+    j--
+    k--
+  }
+};
