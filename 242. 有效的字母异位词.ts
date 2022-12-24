587fe31b7fa6037a7b0864a557923d223697b454
CCherry07 <2405693142@qq.com>
2022-12-24 20:40:55 +0800

    feat: 242. 有效的字母异位词

--- 242. 有效的字母异位词.ts
+++ 242. 有效的字母异位词.ts
@@ -1,25 +1,25 @@
 function isAnagram(s: string, t: string): boolean {
-  let map = new Map()
-  for (let item of s) {
+  const map = new Map()
+  for (const item of s) {
     if (map.has(item)) {
       map.set(item, map.get(item) + 1)
     } else {
       map.set(item, 1)
     }
   }
-  for (let item of t) {
+  for (const item of t) {
     if (map.has(item)) {
       map.set(item, map.get(item) - 1)
     } else {
       return false
     }
   }
-  for (let item of map.values()) {
-    if (item !== 0) {
-      return false
-    }
+
+  for (const item of map.values()) {
+    if (item !== 0) return false
   }
   return true
+
 };
 
 let s = "rat", t = "cat"
