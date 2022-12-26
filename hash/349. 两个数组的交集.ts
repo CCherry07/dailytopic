7b47a9a43857218de2d82216149dddc2b89b23d2
CCherry07 <2405693142@qq.com>
2022-12-26 21:39:52 +0800

    feat: 349. 两个数组的交集

--- hash/349. 两个数组的交集.ts
+++ hash/349. 两个数组的交集.ts
@@ -1,16 +1,17 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
   const set = new Set<number>()
-  let set2: Set<number>;
   if (nums1.length > nums2.length) {
-    set2 = new Set(nums1)
+    let set2: Set<number> | null = new Set<number>(nums1)
     nums2.forEach(num => {
-      set2.has(num) && set.add(num)
+      set2!.has(num) && set.add(num)
     })
+    set2 = null
   } else {
-    set2 = new Set(nums2)
+    let set2: Set<number> | null = new Set(nums2)
     nums1.forEach(num => {
-      set2.has(num) && set.add(num)
+      set2!.has(num) && set.add(num)
     })
+    set2 = null
   }
   return [...set]
 };
