d1a4600b19e2317710fc21e2c99a4f7090dad87e
CCherry07 <2405693142@qq.com>
2022-12-29 20:35:50 +0800

    feat: twoSum

---
+++ hash/1. 两数之和.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      return [map.get(nums[i]), i]
+    } else {
+      map.set(target - nums[i], i)
+    }
+  }
+  return []
+};
