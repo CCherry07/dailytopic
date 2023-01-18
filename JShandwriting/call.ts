da2b2657df39b96cbbaf06d86565a4a355694548
CCherry7 <2405693142@qq.com>
2023-01-18 23:12:45 +0800

    feat:  call object

--- JShandwriting/call.ts
+++ JShandwriting/call.ts
@@ -1,15 +1,18 @@
 // @ts-nocheck
 Function.prototype.myCall = function (context, ...args) {
-  const fn = Symbol('fn')
-  context = context || window
-  context[fn] = this
-  let result = eval(context).fn(...args)
-  delete context[fn]
-  return result
+  if (typeof this !== 'function' || this === null) {
+    throw new TypeError('Error')
+  }
+  const fn = Symbol('fn') // 用Symbol保证fn不会重复
+  context = typeof context === 'object' ? context : new Object(context) // 如果context不是对象，就将其转换为对象
+  context[fn] = this // 将函数挂载到context上
+  let result = eval(context)[fn](...args) // 执行函数
+  delete context[fn] // 删除函数
+  return result // 返回函数执行结果
 }
 
 function foo(...args) {
-  console.log(this.name, args)
+  console.log(this, args)
 }
 
-foo.myCall({ name: 'cherry' }, 'a', 'b', 'c')
+foo.myCall(undefined, 'a', 'b', 'c')
