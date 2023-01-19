81060a6e06281d148bf12f0604248fac8c64c95c
CCherry7 <2405693142@qq.com>
2023-01-20 00:19:24 +0800

    feat:  fix call bug

--- JShandwriting/call.ts
+++ JShandwriting/call.ts
@@ -6,7 +6,7 @@ Function.prototype.myCall = function (context, ...args) {
   const fn = Symbol('fn') // 用Symbol保证fn不会重复
   context = typeof context === 'object' ? context : new Object(context) // 如果context不是对象，就将其转换为对象
   context[fn] = this // 将函数挂载到context上
-  let result = eval(context)[fn](...args) // 执行函数
+  let result = context[fn](...args) // 执行函数
   delete context[fn] // 删除函数
   return result // 返回函数执行结果
 }
