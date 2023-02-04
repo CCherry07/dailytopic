91e8fc0df4e29aa518f31071c531e29352b552de
CCherry07 <2405693142@qq.com>
2023-02-04 22:55:59 +0800

    feat:  cloneDeep strong

--- JShandwriting/cloneDeep.ts
+++ JShandwriting/cloneDeep.ts
@@ -1,4 +1,4 @@
-
+// @ts-nocheck
 const testobj = {
   name: "cherry",
   age: 18,
@@ -9,24 +9,53 @@ const testobj = {
 }
 // @ts-ignore
 testobj.target = testobj
+
+function isObject(target: any) {
+  const type = typeof target
+  return target !== null && (type === 'object' || type === 'function')
+}
+function getType(target: any) {
+  return Object.prototype.toString.call(target);
+}
 function cloneDeep<T>(target: T, weakMap = new WeakMap<any>()): T {
-  if (typeof target === 'object') {
-    const currant: any = Array.isArray(target) ? [] : {}
-    if (weakMap.has(target)) {
-      return weakMap.get(target)
-    }
-    weakMap.set(target, currant)
-    for (const key in target) {
-      if (Object.prototype.hasOwnProperty.call(target, key)) {
-        currant[key] = cloneDeep(target[key], weakMap)
-      }
-    }
-    return currant
-  } else {
-    return target
+  if (!isObject(target)) {
+    return target;
   }
+  // 解决循环引用
+  if (weakMap.has(target)) {
+    return weakMap.get(target)
+  }
+  let currant: any
+  weakMap.set(target, currant)
+  // clone 其他类型
+  const type = getType(target)
+  // Set
+  if (type === '[object Set]') {
+    currant = new Set()
+    target.forEach((item: any) => {
+      currant.add(cloneDeep(item, weakMap))
+    })
+    return currant
+  }
+  // Map
+  if (type === '[object Map]') {
+    currant = new Map()
+    target.forEach((item: any, key: any) => {
+      currant.set(key, cloneDeep(item, weakMap))
+    })
+    return currant
+  }
+  // clone 数组 和 对象
+  currant = Array.isArray(target) ? [] : {}
+  for (const key in target) {
+    if (Object.prototype.hasOwnProperty.call(target, key)) {
+      currant[key] = cloneDeep(target[key], weakMap)
+    }
+  }
+  return currant
 }
 const newobj = cloneDeep(testobj)
 newobj.address.city = '杭州'
 console.log(testobj.address);
+
 
