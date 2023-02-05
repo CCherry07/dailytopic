2958c7ddb18f7ccafa45be3577869eb1688a636c
CCherry07 <2405693142@qq.com>
2023-02-05 20:09:05 +0800

    feat:  前序遍历的迭代法

--- tree/144. 二叉树的前序遍历.ts
+++ tree/144. 二叉树的前序遍历.ts
@@ -12,3 +12,20 @@ function preorderTraversal(root: TreeNode | null): number[] {
   }
   return result
 };
+
+
+function preorderTraversalStack(root: TreeNode | null): number[] {
+  let result: number[] = []
+  let stack: TreeNode[] = []
+  let current = root
+  while (current || stack.length) {
+    while (current) { // 一直往左走
+      result.push(current.val) // 先序遍历，先访问节点，再访问左子树，最后访问右子树
+      stack.push(current) // 入栈
+      current = current.left // 一直往左走
+    }
+    current = stack.pop()! // 出栈
+    current = current.right // 一直往右走
+  }
+  return result
+}
