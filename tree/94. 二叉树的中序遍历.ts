5b65baa3b8bdc7978c8ec5fce13ba9fcf216d2fd
CCherry07 <2405693142@qq.com>
2023-02-06 20:12:30 +0800

    feat:  中序遍历的迭代法

--- tree/94. 二叉树的中序遍历.ts
+++ tree/94. 二叉树的中序遍历.ts
@@ -11,3 +11,20 @@ function inorderTraversal(root: TreeNode | null): number[] {
     find(node?.right, result)
   }
 };
+
+
+function inorderTraversalStack(root: TreeNode | null): number[] {
+  let result: number[] = []
+  let stack: TreeNode[] = []
+  let current = root
+  while (current || stack.length) {
+    while (current) { // 一直往左走
+      stack.push(current) // 入栈
+      current = current.left // 一直往左走
+    }
+    current = stack.pop()! // 出栈
+    result.push(current.val) // 中序遍历，先访问左子树，再访问节点，最后访问右子树
+    current = current.right // 一直往右走
+  }
+  return result
+}
