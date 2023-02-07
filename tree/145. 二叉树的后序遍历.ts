9f60df7afb6d93cc9b2ad76c7f51d0e1b7273115
CCherry07 <2405693142@qq.com>
2023-02-07 20:12:40 +0800

    feat:  后序遍历的迭代法

--- tree/145. 二叉树的后序遍历.ts
+++ tree/145. 二叉树的后序遍历.ts
@@ -11,3 +11,19 @@ function postorderTraversal(root: TreeNode | null): number[] {
   }
   return result
 };
+
+function postorderTraversalStack(root: TreeNode | null): number[] {
+  let result: number[] = []
+  let stack: TreeNode[] = []
+  let current = root
+  while (current || stack.length) {
+    while (current) { // 一直往左走
+      result.unshift(current.val) // 后序遍历，先访问左子树，再访问右子树，最后访问节点
+      stack.push(current) // 入栈
+      current = current.right // 一直往右走
+    }
+    current = stack.pop()! // 出栈
+    current = current.left // 一直往左走
+  }
+  return result
+}
