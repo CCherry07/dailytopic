7165215eacb7f9b74ac8fdb8a335e0353260ca68
CCherry07 <2405693142@qq.com>
2023-02-08 23:10:22 +0800

    feat:  二叉树的层序遍历

--- tree/102. 二叉树的层序遍历.ts
+++ tree/102. 二叉树的层序遍历.ts
@@ -4,11 +4,11 @@ function levelOrder(root: TreeNode | null): number[][] {
   let Queue: TreeNode[] = []
   if (root !== null) Queue.push(root);
   while (Queue.length) {
-    let size = Queue.length
-    const tier:number[] = []
-    while (size--) {
-      const shiftNode = Queue.shift() as TreeNode
-      tier.push(shiftNode.val)
+    let size = Queue.length // 本层节点数
+    const tier: number[] = [] // 本层节点值
+    while (size--) { // 本层节点数
+      const shiftNode = Queue.shift() as TreeNode // 出队列
+      tier.push(shiftNode.val) // 本层节点值
       if (shiftNode.left) Queue.push(shiftNode.left)
       if (shiftNode.right) Queue.push(shiftNode.right)
     }
@@ -16,3 +16,27 @@ function levelOrder(root: TreeNode | null): number[][] {
   }
   return result
 };
+
+
+const a1 = [{ a: 1, b: 1 }, { a: 2, b: 2 }, { a: 3, b: 3 }]
+const a2 = [{ a: 1, c: 1 }, { a: 3, c: 3 }, { a: 4, c: 4 }]
+
+
+type PublicAttribute<A extends O, B extends O> = {
+  [K in keyof A & keyof B]: A[K] extends B[K] ? K : never
+}
+
+type O = Record<string, number>
+
+function merge<A extends O, B extends O>(key: keyof PublicAttribute<A, B>, a1: A[], a2: B[]) {
+  return a1.map(item => {
+    // @ts-ignore
+    const target = a2.find(item2 => item[key] === item2[key])
+    if (target) {
+      return { ...item, ...target }
+    }
+  }).filter(Boolean)
+}
+
+console.log(merge('a', a1, a2)); // [ { a: 1, b: 1, c: 1 }, { a: 3, b: 3, c: 3 } ]
+
