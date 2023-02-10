acea7f0e90a80a784ac8f6756acf2bd64348a8c8
CCherry07 <2405693142@qq.com>
2023-02-10 22:51:05 +0800

    feat:  二叉树的最小深度

--- tree/111. 二叉树的最小深度.ts
+++ tree/111. 二叉树的最小深度.ts
@@ -6,7 +6,7 @@ import { TreeNode } from "./type";
 
 function minDepth(root: TreeNode | null): number {
   if (!root) return 0
-  if (root.left === null && root.right !== null) return minDepth(root.right) + 1
-  if (root.right === null && root.left !== null) return minDepth(root.left) + 1
-  return Math.min(minDepth(root.left), minDepth(root.right)) + 1
+  if (root.left === null && root.right !== null) return minDepth(root.right) + 1 // 有右子树，但是没有左子树
+  if (root.right === null && root.left !== null) return minDepth(root.left) + 1 // 有左子树，但是没有右子树
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1 // 有左右子树或者没有子树，都是返回最小的那个
 };
