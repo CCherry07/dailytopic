faf660057e14d14da58005430412295bba014984
CCherry07 <2405693142@qq.com>
2023-02-10 22:50:48 +0800

    feat:  二叉树的最大深度

--- tree/104. 二叉树的最大深度.ts
+++ tree/104. 二叉树的最大深度.ts
@@ -2,6 +2,6 @@ import { TreeNode } from "./type"
 
 function maxDepth(root: TreeNode | null): number {
   if (root === null) return 0
-  // 遍历左右子树
+  // 递归左右子树,每次递归返回的是左右子树的最大深度 都加 1
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1 // 递归最后一层的时候，返回 1，然后一层一层返回
 };
