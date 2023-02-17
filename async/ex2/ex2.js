e2bea751b256d6fe31536e0e3408bb1c8cc57b86
CCherry07 <2405693142@qq.com>
2023-02-17 20:07:17 +0800

    feat:  完成ex2 thunk 练习

--- async/ex2/ex2.js
+++ async/ex2/ex2.js
@@ -1,4 +1,4 @@
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	var fake_responses = {
 		"file1": "The first text",
 		"file2": "The middle text",
@@ -8,9 +8,9 @@ function fakeAjax(url,cb) {
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -21,7 +21,39 @@ function output(text) {
 
 function getFile(file) {
 	// what do we do here?
+	let state, fn
+	fakeAjax(file, function (text) {
+		// what do we do here?
+		if (typeof fn === 'function') {
+			fn(text)
+		} else {
+			state = text
+		}
+	})
+
+	return function (cb) {
+		if (typeof state === 'undefined') {
+			fn = cb
+		} else {
+			cb(state)
+		}
+	}
 }
 
 // request all files at once in "parallel"
+
+const th1 = getFile("file1");
+const th2 = getFile("file2");
+const th3 = getFile("file3");
 // ???
+
+th1(function (text1) {
+	output(text1);
+	th2(function (text2) {
+		output(text2);
+		th3(function (text3) {
+			output(text3);
+			output("Complete!");
+		});
+	});
+});
