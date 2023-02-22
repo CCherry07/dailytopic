0dc81fe6330b0f47318fe8b0f71f07a65aaac946
CCherry07 <2405693142@qq.com>
2023-02-22 16:14:40 +0800

    feat:  ex5

--- async/ex5/ex5.js
+++ async/ex5/ex5.js
@@ -1,4 +1,4 @@
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	var fake_responses = {
 		"file1": "The first text",
 		"file2": "The middle text",
@@ -8,9 +8,9 @@ function fakeAjax(url,cb) {
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -21,7 +21,21 @@ function output(text) {
 
 function getFile(file) {
 	// what do we do here?
+	return ASQ(function (done) {
+		fakeAjax(file, done);
+	});
 }
+
+getFile('file1')
+	.val(output)
+	.seq(getFile('file2'))
+	.val(output)
+	.seq(getFile('file3'))
+	.val(output)
+	.val(function () {
+		output('Complete!');
+	});
+
 
 // request an array of files at once in "parallel"
 // ???
