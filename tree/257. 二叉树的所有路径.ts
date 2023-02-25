3e4eb8f7acaefbce2f814ebf4e2cc712881a6feb
CCherry7 <2405693142@qq.com>
2023-02-25 19:38:22 +0800

    feat: 二叉树的所有路径

--- tree/257. 二叉树的所有路径.ts
+++ tree/257. 二叉树的所有路径.ts
@@ -1,17 +1,17 @@
 import { TreeNode } from './type';
 function binaryTreePaths(root: TreeNode | null): string[] {
-  const res:string[] = []
+  const result: string[] = []
   if (!root) return []
   function def(node: TreeNode, path: string) {
     const { left, right } = node
-    if (!left && !right) return res.push(path)
+    if (!left && !right) return result.push(path) // 这里的 path 是上一层的 path
     if (left) {
-      def(left, `${path}->${left.val}`)
+      def(left, `${path}->${left.val}`) // 将 path 以及left value 拼接 传递给下一层
     }
     if (right) {
-      def(right, `${path}->${right.val}`)
+      def(right, `${path}->${right.val}`) // 将 path 以及right value 拼接 传递给下一层
     }
   }
   def(root, `${root.val}`)
-  return res
+  return result
 };
