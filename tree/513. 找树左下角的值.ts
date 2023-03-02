0a9ee57ec5cfdc1a2701ccaf65e168a222db4cc8
CCherry07 <2405693142@qq.com>
2023-03-02 22:36:34 +0800

    feat: 找树左下角的值 深度优先遍历

--- tree/513. 找树左下角的值.ts
+++ tree/513. 找树左下角的值.ts
@@ -21,4 +21,24 @@ function findBottomLeftValue(root: TreeNode): number {
 
 
 // 深度优先遍历
+{
 
+  function findBottomLeftValue(root: TreeNode): number {
+    let height = 0
+    let left = root
+    function defs(node: TreeNode, depth: number) {
+      if (!node.left && !node.right) {
+        if (depth > height) {
+          height = depth
+          left = node
+        }
+      }
+
+      if (node.left) defs(node.left, depth + 1)
+      if (node.right) defs(node.right, depth + 1)
+    }
+    defs(root, height)
+    return left.val
+  };
+
+}
