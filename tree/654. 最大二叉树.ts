6d84cac2f61e6671a843cebed09e207c467b2d1e
CCherry07 <2405693142@qq.com>
2023-03-06 22:53:24 +0800

    feat:  654. 最大二叉树 简写

--- tree/654. 最大二叉树.ts
+++ tree/654. 最大二叉树.ts
@@ -11,7 +11,7 @@ function constructMaximumBinaryTree(nums: number[]): TreeNode | null {
   node.left = constructMaximumBinaryTree(nums.slice(0, maxValIdx))
   node.left = constructMaximumBinaryTree(nums.slice(maxValIdx + 1)) // 越过当前已经操作的结点
 
-  // return new TreeNode(maxValue, constructMaximumBinaryTree(nums.slice(0, maxValueIdx)), constructMaximumBinaryTree(nums.slice(maxValueIdx + 1)))
+  // return new TreeNode(maxVal, constructMaximumBinaryTree(nums.slice(0, maxValIdx)), constructMaximumBinaryTree(nums.slice(maxValIdx + 1)))
 
   return node
 }
