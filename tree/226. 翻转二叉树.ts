dbe4c7a84ad590efef036230fa665fd142a4064a
CCherry07 <2405693142@qq.com>
2023-04-15 22:18:13 +0800

    feat:226. 翻转二叉树

--- tree/226. 翻转二叉树.ts
+++ tree/226. 翻转二叉树.ts
@@ -3,5 +3,5 @@ import { TreeNode } from "./type"
 function invertTree(root: TreeNode | null):TreeNode | null {
   if(root === null) return root as null
   [root.left,root.right] = [invertTree(root.right),invertTree(root.left)]
-  return root as TreeNode
+  return root
 };
