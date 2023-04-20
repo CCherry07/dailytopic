0a4c2e1e101946a15e00914368658f468217292d
CCherry07 <2405693142@qq.com>
2023-04-20 11:01:23 +0800

    feat: combinePromises

--- JShandwriting/combinePromises.ts
+++ JShandwriting/combinePromises.ts
@@ -18,7 +18,7 @@ const Three = function (num: number) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(num * 3);
-    }, 1000);
+    }, 3000);
   })
 }
 
@@ -27,22 +27,22 @@ interface Config {
   current?: number,
 }
 
-function combinePromises(config: Config, ...args: ((...p: any[]) => Promise<any>)[]) {
+function combinePromises<T = any>(config: Config, ...args: ((...p: any[]) => Promise<any>)[]) {
   const { initSate, current = args.length } = config
-  return new Promise((resolve, reject) => {
+  return new Promise<T>((resolve, reject) => {
+    // @ts-ignore
     args.reduce(function combine(chain, asyncFunc, currentIndex) {
-      const nextChain = chain.then((response) => {
-        if (currentIndex === current - 1) {
-          resolve(nextChain)
-        }
-        return asyncFunc(response)
-      }, reject)
-      return nextChain
+      if (currentIndex === current) {
+        resolve(chain)
+        return // 这里必须要return 否则会继续执行下面的then
+      }
+      return  chain.then(asyncFunc, reject)
     }, Promise.resolve((typeof initSate === 'function' ? initSate() : initSate)))
   })
 }
 
 // 一般写法
+
 // single(20).then((res: any) => {
 //   double(res).then((res: any) => {
 //     Three(res).then(res => {
@@ -50,10 +50,11 @@ function combinePromises(config: Config, ...args: ((...p: any[]) => Promise<any>
 //     })
 //   })
 // })
-// 采用组合函数 传入初始值和多个函数
-combinePromises({
-  initSate: async () => 20,
-  current: 2
+
+// 采用组合函数 传入初始值和多个promise函数
+combinePromises<number>({
+  initSate: async () => 20, // 初始值
+  current: 2 // 到几个promise函数就停止
 }, single, double, Three)
   .then(res => {
     console.log(res);
